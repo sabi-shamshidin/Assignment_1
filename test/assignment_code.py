34599d96b1f7cb04cd01e4dfa48dce30e0831e08
sabi-shamshidin <82763714+sabi-shamshidin@users.noreply.github.com>
2021-09-26 23:22:15 +0600

    Update assignment_code.py

--- test/assignment_code.py
+++ test/assignment_code.py
@@ -1,13 +1,13 @@
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
-import pandas
+import pandas as pd
 
 class market_cap:
     def __init__(self, n):
         self.n = n    
     def top_n(self):
         coins_m = cg.get_coins_markets(vs_currency = 'usd')
-        df_m = pandas.DataFrame(coins_m, columns=['id','market_cap'])
+        df_m = pd.DataFrame(coins_m, columns=['id','market_cap'])
         df_top = df_m.nlargest(self.n, 'market_cap')
         print(df_top)
 
